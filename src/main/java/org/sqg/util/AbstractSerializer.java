8d2e4bf129e2f8941800a08b7783b9f86c3bd372
Quangang Sheng <shengquangang2015@163.com>
2018-01-18 17:43:04 +0800

    remove debug log in mina server

--- src/main/java/org/sqg/util/AbstractSerializer.java
+++ src/main/java/org/sqg/util/AbstractSerializer.java
@@ -6,29 +6,49 @@ import java.io.IOException;
 
 public abstract class AbstractSerializer implements Serializer {
 
-    public final <T> byte[] serialize(final T objectGraph) {
-        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
-            serialize(objectGraph, os);
-            return os.toByteArray();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	public final <T> byte[] serialize(final T objectGraph) {
+		try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+			serialize(objectGraph, os);
+			return os.toByteArray();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public final <T> T deserialize(final byte[] bytes, final Class<T> type) {
-        try (ByteArrayInputStream is = new ByteArrayInputStream(bytes)) {
-            return deserialize(is, type);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	public final <T> T deserialize(final byte[] bytes, int offset, int length, final Class<T> type) {
+		try (ByteArrayInputStream is = new ByteArrayInputStream(bytes, offset, length)) {
+			return deserialize(is, type);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public final Object deserialize(final byte[] bytes)
-            throws ClassNotFoundException {
-        try (ByteArrayInputStream is = new ByteArrayInputStream(bytes)) {
-            return deserialize(is);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	public final <T> T deserialize(final byte[] bytes, final Class<T> type) {
+		try (ByteArrayInputStream is = new ByteArrayInputStream(bytes)) {
+			return deserialize(is, type);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public final Object deserialize(final byte[] bytes) throws ClassNotFoundException {
+		try (ByteArrayInputStream is = new ByteArrayInputStream(bytes)) {
+			return deserialize(is);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public final Object deserialize(final byte[] bytes, int offset, int length) throws ClassNotFoundException {
+		try (ByteArrayInputStream is = new ByteArrayInputStream(bytes, offset, length)) {
+			return deserialize(is);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- src/main/java/org/sqg/util/Serializer.java
+++ src/main/java/org/sqg/util/Serializer.java
@@ -6,16 +6,19 @@ import java.io.OutputStream;
 
 public interface Serializer {
 
-    <T> void serialize(T objectGraph, OutputStream os) throws IOException;
+	<T> void serialize(T objectGraph, OutputStream os) throws IOException;
 
-    <T> byte[] serialize(T objectGraph);
+	<T> byte[] serialize(T objectGraph);
 
-    <T> T deserialize(InputStream is, Class<T> type) throws IOException;
+	<T> T deserialize(InputStream is, Class<T> type) throws IOException;
 
-    <T> T deserialize(byte[] bytes, Class<T> type);
+	<T> T deserialize(byte[] bytes, Class<T> type);
 
-    Object deserialize(InputStream is) throws IOException,
-            ClassNotFoundException;
+	<T> T deserialize(byte[] bytes, int offset, int length, Class<T> type);
 
-    Object deserialize(byte[] bytes) throws ClassNotFoundException;
+	Object deserialize(InputStream is) throws IOException, ClassNotFoundException;
+
+	Object deserialize(byte[] bytes) throws ClassNotFoundException;
+
+	Object deserialize(byte[] bytes, int offset, int length) throws ClassNotFoundException;
 }
